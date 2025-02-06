097525a629bcc9317b5736393914a9d6b8010984
kirisame19 <kirisamekari19@gmail.com>
2025-02-06 14:59:26 +0900

    プロジェクトを作成した｡開発環境構築中

---
+++ backend/todo/serializers.py
@@ -0,0 +1,10 @@
+﻿from rest_framework import serializers
+from .models import Todo
+
+
+class TodoSerializer(serializers.Serializer):
+    """Todoシリアライズ"""
+
+    class Meta:
+        model = Todo
+        fields = "__all__"
---
+++ backend/todo/urls.py
@@ -0,0 +1,13 @@
+﻿from django.urls import path
+
+from . import views
+
+app_name = "todo"
+
+urlpatterns = [
+    path(
+        "todos",
+        views.TodoViewSet.as_view({"get": "list", "post": "create"}),
+        name="todos",
+    ),
+]
